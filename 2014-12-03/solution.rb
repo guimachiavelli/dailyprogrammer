9011d83b8e95ce0339f01116a835603516f7c439
gui machiavelli <hey@guimachiavelli.com>
2015-02-08 18:50:31 +0000

    checking if start/end points are valid

--- 2014-12-03/solution.rb
+++ 2014-12-03/solution.rb
@@ -37,6 +37,59 @@ class SpaceMap
         map
     end
 
+    def coordinate_content(coord)
+        return @map[coord[0]][coord[1]]
+    end
+
+    def safe_point?(coord)
+        near_gravity_well?(coord) or is_obstacle?(coord)
+    end
+
+    def is_obstacle?(coord)
+        point = @map[coord[0]][coord[1]]
+        point == 'A' or point == 'G'
+    end
+
+    def near_gravity_well?(coord)
+        x, y = coord
+        area = iterate_area(x,y)
+
+        area.each do |point|
+            return true if @map[point[0]][point[1]] == 'G'
+        end
+
+        return false
+    end
+
+    def iterate_area(x,y)
+        dirs = [
+            [1,   0],
+            [1,   1],
+            [1,  -1],
+            [-1,  0],
+            [-1,  1],
+            [-1, -1],
+            [0,   1],
+            [0,  -1],
+            [0,   0]
+        ]
+
+        area = []
+
+        dirs.each do |dir|
+            new_x = x + dir[0]
+            new_y = y + dir[1]
+            area.push([new_x, new_y]) if valid_index?(new_x, new_y, @map.length)
+        end
+
+        area
+    end
+
+    def valid_index?(x, y, length)
+        x > -1 and x < length and
+        y > -1 and y < length
+    end
+
     def to_s
         @map.map do |row|
             row.join
@@ -45,6 +98,22 @@ class SpaceMap
 
 end
 
-map = SpaceMap.new(9)
+class Probe
+    def initialize(start, finish)
+        @start = start
+        @finish = finish
+    end
+end
 
-puts map.to_s
+class SpaceProbe
+    def initialize(size, from, to)
+        @map = SpaceMap.new(10)
+
+        raise 'Obstacle on starting point' unless @map.safe_point?(from)
+        raise 'Obstacle on finish point' unless @map.safe_point?(to)
+
+        @probe = Probe.new(from, to)
+    end
+end
+
+space_probe = SpaceProbe.new 10,[0,0],[9,9]
